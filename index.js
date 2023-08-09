abd44f600d747b98fda103ce26c2123dab100233
Micaheal Johnston <36141218+md9j@users.noreply.github.com>
2023-08-09 10:42:46 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+import express from "express";
+import bodyParser from "body-parser";
+
+const app = express();
+const port = 3000;
+
+// Set the view engine to EJS
+app.set("view engine", "ejs");
+
+// Serve static files from the "public" directory
+app.use(express.static("public"));
+
+// Parse URL-encoded form data using bodyParser middleware
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Start the server and listen on the specified port
+app.listen(port, () => {
+    console.log(`It's me, port ${port}, I'm the listening port it's me`);
+});
+
+// Route handlers
+app.get("/", (req, res) => {
+    res.render("index.ejs");
+});
