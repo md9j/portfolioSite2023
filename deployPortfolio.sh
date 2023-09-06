0dfa536e535369590da643242891fc092123feda
Micaheal Johnston <36141218+md9j@users.noreply.github.com>
2023-09-06 17:11:48 -0600

    no vars

--- deployPortfolio.sh
+++ deployPortfolio.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ssh root@45.79.109.251 'bash -s'
-docker pull $DOCKER_REGISTRY/$DOCKER_IMAGE_PORTFOLIO:latest
-docker container stop $DOCKER_CONTAINER_PORTFOLIO
-docker rm $DOCKER_CONTAINER_PORTFOLIO
-docker run -d -p 443:3000 --name $DOCKER_CONTAINER_PORTFOLIO $DOCKER_REGISTRY/$DOCKER_IMAGE_PORTFOLIO:latest
+docker pull mjohnst/portfolio2023:latest
+docker stop portfolio
+docker rm portfolio
+docker run -d -p 443:3000 --name portfolio mjohnst/portfolio2023:latest
