553f2aac89cd96bc7a0d9bed249e5dacbcb5f810
Micaheal Johnston <36141218+md9j@users.noreply.github.com>
2023-09-08 12:15:42 -0600

    add pipline rt

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ app.listen(port, () => {
     console.log(`It's me, port ${port}, I'm the listening port it's me`);
 });
 
-// Route handlers
+// Route handlers ********************
 app.get('/', (req, res) => {
     res.render('index.ejs');
 });
@@ -39,6 +39,11 @@ app.get('/', (req, res) => {
 // UAP route
 app.get('/uap', (req, res) => {
     res.render('uap.ejs');
+});
+
+// Pipeline route
+app.get('/pipeline', (req, res) => {
+    res.render('pipeline.ejs');
 });
 
 // Sudoku route
