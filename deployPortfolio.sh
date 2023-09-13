67d8b88ba1f8adaca15a8dfb74be48888c8d9e64
md9j <36141218+md9j@users.noreply.github.com>
2023-09-13 12:45:55 -0600

    Main (#15)

--- deployPortfolio.sh
+++ deployPortfolio.sh
@@ -4,4 +4,4 @@ ssh root@45.79.109.251 'bash -s'
 docker pull mjohnst/portfolio2023:latest
 docker stop portfolio
 docker rm portfolio
-docker run -d -p 443:3000 --name portfolio mjohnst/portfolio2023:latest
+docker run -d -e PORT=3000 --name portfolio mjohnst/portfolio2023:latest
