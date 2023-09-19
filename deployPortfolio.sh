554630fb7d4fa6c06804362e0c88473679a03c7c
Micaheal Johnston <36141218+md9j@users.noreply.github.com>
2023-09-18 20:17:32 -0600

    update docker commands

--- deployPortfolio.sh
+++ deployPortfolio.sh
@@ -2,3 +2,6 @@
 
 ssh root@45.79.109.251 'bash -s'
 docker pull mjohnst/portfolio2023:latest
+docker stop portfolio 
+docker rm portfolio
+docker run -d -p 3000:3000 --name portfolio  mjohnst/portfolio2023:latest
