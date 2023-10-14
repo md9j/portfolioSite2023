2b0e77e98f170b2be125f6a3d129af1d08c3dc35
Micaheal Johnston <36141218+md9j@users.noreply.github.com>
2023-10-14 14:12:22 -0600

    update token and api key

--- authorizationConfig.js
+++ authorizationConfig.js
@@ -1,4 +1,4 @@
 export const username = 'portfolio';
 export const password = 'showMEtheSECRETS';
-export const apiKey = '671a17ee-4950-46d7-a443-138dfc7a571d';
-export const bearerToken = 'bc449690-bf45-4de3-8c52-fabac6e787b0';
+export const apiKey = '973a2163-6eed-4385-87e5-5e4e4299d0d2';
+export const bearerToken = '4c1be614-1954-409b-8df0-2f0fd8f367d1';
