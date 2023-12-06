7f651798efd5409b527a1eb4c7df979d3b0a3995
Micaheal Johnston <36141218+md9j@users.noreply.github.com>
2023-12-06 09:00:29 -0700

    update credentials

--- authorizationConfig.js
+++ authorizationConfig.js
@@ -1,4 +1,4 @@
 export const username = 'portfolio';
 export const password = 'showMEtheSECRETS';
-export const apiKey = '206a3c1a-ea3a-4993-acb6-43b98a8f1f34';
-export const bearerToken = '36026ecc-891f-40d0-9b2a-7fbaf820ac7c'
+export const apiKey = '17755ce1-841a-4d74-9ae2-0bcbc4f6e017';
+export const bearerToken = '91c0702b-baaf-4964-bbc6-d6792febedf4'
