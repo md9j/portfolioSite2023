e40043dcf50c87d24085e0294e5cf9e8dd6fcce0
md9j <36141218+md9j@users.noreply.github.com>
2024-01-12 10:33:09 -0700

    Update the creds (#68)

--- authorizationConfig.js
+++ authorizationConfig.js
@@ -1,4 +1,4 @@
 export const username = 'portfolio';
 export const password = 'showMEtheSECRETS';
-export const apiKey = '00bb906c-adbf-461e-b60f-a80f82e33cfd';
-export const bearerToken = 'd27beee8-55ff-4230-8c5d-a5b9c21f8295'
+export const apiKey = 'be2d98d6-9d25-426b-8052-3b2b594b8e4b';
+export const bearerToken = '281a6e5e-eb3c-4561-b5e7-f30cbb21ec94'
