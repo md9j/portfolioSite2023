1e0c256d909259cf37ceaccfc3973d8bc735bc37
Micaheal Johnston <36141218+md9j@users.noreply.github.com>
2024-01-22 10:14:14 -0700

    Update authorizationConfig.js with new API keys

--- authorizationConfig.js
+++ authorizationConfig.js
@@ -1,4 +1,4 @@
 export const username = 'portfolio';
 export const password = 'showMEtheSECRETS';
-export const apiKey = 'be2d98d6-9d25-426b-8052-3b2b594b8e4b';
-export const bearerToken = '281a6e5e-eb3c-4561-b5e7-f30cbb21ec94'
+export const apiKey = '5b68e38e-fddf-418b-99c9-faf580bf39da';
+export const bearerToken = 'db29bfb6-021f-4dcc-b936-6eecbedab775'
