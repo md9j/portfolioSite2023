c48ce1a39d43d8815f2f88f46ff84b74195ab046
md9j <36141218+md9j@users.noreply.github.com>
2024-02-08 13:19:35 -0700

    maintainin' that cred (#72)

--- authorizationConfig.js
+++ authorizationConfig.js
@@ -1,4 +1,4 @@
 export const username = 'portfolio';
 export const password = 'showMEtheSECRETS';
-export const apiKey = '655401a8-5465-447b-b360-8d031cd43615';
-export const bearerToken = '363eb4aa-9672-4509-9d86-7b9b637d050b'
+export const apiKey = '61a04cf7-243e-49c8-a82d-51295ef81004';
+export const bearerToken = '87b294fe-a255-4f77-a8a2-b14ba5597f7d'
