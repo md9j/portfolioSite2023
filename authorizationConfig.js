85e59532782c0ed5fc1d222324343ddf9e08d45f
md9j <36141218+md9j@users.noreply.github.com>
2024-02-19 16:18:44 -0700

    you know the deal (#73)

--- authorizationConfig.js
+++ authorizationConfig.js
@@ -1,4 +1,4 @@
 export const username = 'portfolio';
 export const password = 'showMEtheSECRETS';
-export const apiKey = '61a04cf7-243e-49c8-a82d-51295ef81004';
-export const bearerToken = '87b294fe-a255-4f77-a8a2-b14ba5597f7d'
+export const apiKey = '6bf6f7c1-3e09-4dcf-836b-1bfc426694ae';
+export const bearerToken = '5a89a3e9-6dd1-413b-b2fc-d92c0817335c'
