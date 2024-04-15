8787e6cc677c3bb1d468a383e3a206cc39ff720b
md9j <36141218+md9j@users.noreply.github.com>
2024-04-15 15:37:55 -0600

    new credentials and certs (#78)

--- authorizationConfig.js
+++ authorizationConfig.js
@@ -1,4 +1,4 @@
 export const username = 'portfolio';
 export const password = 'showMEtheSECRETS';
-export const apiKey = '5069d2b5-0033-4eed-b17f-1c615b466e98';
-export const bearerToken = 'd64def6f-f98c-4daa-bbb8-783c121386e1'
+export const apiKey = 'fab65159-33ef-4650-aeb5-e42bbfa0c364';
+export const bearerToken = '0c5e81dd-24a3-4401-8602-97730fc30e15'
