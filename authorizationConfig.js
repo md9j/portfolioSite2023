d85f0bb8b2b0d771ac5283ad0b2da04cdf3fdd47
md9j <36141218+md9j@users.noreply.github.com>
2024-08-27 14:44:58 -0600

    update secrets api credentials (#82)

--- authorizationConfig.js
+++ authorizationConfig.js
@@ -1,4 +1,4 @@
-export const username = 'portfolio';
-export const password = 'showMEtheSECRETS';
-export const apiKey = '05b86e22-dd2f-452c-9a70-a411cf3acaae';
-export const bearerToken = '43150c3c-a943-462e-85d4-5681f1569d59'
+export const username = "portfolio";
+export const password = "showMEtheSECRETS";
+export const apiKey = "a4e56164-cbaa-43a4-ab4b-27770f4ae898";
+export const bearerToken = "29c3ba54-9f95-4e89-ab50-18030656e9df";
