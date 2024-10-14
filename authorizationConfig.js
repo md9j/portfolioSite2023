6906a88a60523b6f690ea668adab79c734cb7867
md9j <36141218+md9j@users.noreply.github.com>
2024-10-14 11:08:22 -0600

    what else could it be..... credentials   (#90)

--- authorizationConfig.js
+++ authorizationConfig.js
@@ -1,4 +1,4 @@
 export const username = "portfolio";
 export const password = "showMEtheSECRETS";
-export const apiKey = "a4e56164-cbaa-43a4-ab4b-27770f4ae898";
-export const bearerToken = "29c3ba54-9f95-4e89-ab50-18030656e9df";
+export const apiKey = "61629cd3-c804-431e-b22e-febd8159d9ec";
+export const bearerToken = "ebc7a3ca-1a00-4a0f-833d-219755eb8b07";
